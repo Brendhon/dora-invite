8b9fcfb1bdeca116cdd8e5190aa2e676bc06df68
Brendhon <brendhon.e.c.m@gmail.com>
2025-05-25 16:52:00 -0300

    Fix message formatting for better alignment in confirmation dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,11 +51,11 @@ export default function Home() {
     if (selectedDay && selectedMovie && selectedSession) {
       message = `¡Hola! 🥳 Eu escolhi um filme pra nossa aventura no Cineart! 🍿
 
-    🎬 *Filme:* ${selectedMovie.title}
-    📅 *Dia:* ${getWeekday(selectedDay)}
-    ⏰ *Hora:* ${selectedSession}
+  🎬 *Filme:* ${selectedMovie.title}
+  📅 *Dia:* ${getWeekday(selectedDay)}
+  ⏰ *Hora:* ${selectedSession}
 
-    Vamos nessa? Vai ser super divertido! 🎒✨`;
+  Vamos nessa? Vai ser super divertido! 🎒✨`;
     }
 
     const phone = "5535997164703";
